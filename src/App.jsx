05283baa9b268022b2054d691b4b8c773f238d9b
Juan Esteban Pérez <43039970+juanez1999@users.noreply.github.com>
2026-02-19 19:11:00 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Form } from './components/Form/Form';
+import { ToDo } from './components/ToDo/ToDo';
+
+function App() {
+  return (
+    <>
+      <ToDo />
+      {/* <Card title="hola" /> */}
+    </>
+  );
+}
+
+export default App;
