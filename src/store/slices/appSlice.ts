9fb03e8e58f066123df4870faa2a9766e7db6ad0
Juan Sebastian Tovar Monje <1006072266@u.icesi.edu.co>
2026-04-27 18:32:48 -0500

    initialcomit

--- src/store/slices/appSlice.ts
+++ src/store/slices/appSlice.ts
@@ -1,22 +1,33 @@
+
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import data from "../../data/data.json"
+import type { InitialWorkspaces, Workspace } from '../../types/WorkspaceTypes';
 
-type AppState = {
-	status: string;
+
+const initialState: InitialWorkspaces = {
+	workspace: [...data],
+	reserved: []
 };
 
-const initialState: AppState = {
-	status: 'ready',
-};
-
-const appSlice = createSlice({
-	name: 'app',
+const workspaceSlice = createSlice({
+	name: 'workspace',
 	initialState,
 	reducers: {
-		setStatus: (state, action: PayloadAction<string>) => {
-			state.status = action.payload;
-		},
+		handleReservation: (state, action: PayloadAction<{workspace: Workspace}>)=>{
+			 const exists =state.reserved.some(
+				(item)=>item.id === action.payload.workspace.id
+			 )
+			 if (!exists){
+				state.reserved.push(action.payload.workspace)
+			 } else {
+				state.reserved= state.reserved.filter(
+					(item) => item.id !== action.payload.workspace.id
+				)
+			 }
+		}
+
 	},
 });
 
-export const { setStatus } = appSlice.actions;
-export default appSlice.reducer;
+export const { handleReservation } = workspaceSlice.actions;
+export default workspaceSlice.reducer;
